f1ee730ac167e77ace1dd387ea92e2a1f5f57056
Jonathan <jonrhoads22@gmail.com>
2021-03-26 13:30:48 -0500

    Adding EF and initial migration

---
+++ WildProducts/Models/StoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WildProducts.Models
+{
+    public class StoreDbContext: DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options)
+              : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
